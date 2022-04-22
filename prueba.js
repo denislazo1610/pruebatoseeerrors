81aa8371548d129884ba1b7eba5eaee259b89727
Denis Lazo <denislazo1610@gmail.com>
2022-04-22 16:02:27 -0600

    assignment1

--- prueba.js
+++ prueba.js
@@ -3,10 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Jordi Castro");
 });
-// this is a test
-// app.use("/", require("./routes"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
